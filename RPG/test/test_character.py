ddc99f8e6f3dd7cc0b10480189aeeac126ddd7eb
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-03-21 16:11:20 +0100

    !

--- RPG/test/test_character.py
+++
@@ -1,6 +0,0 @@
-from character import Character
-
-# vérifie qu'un personnage a 100pt de vie à sa création
-def test_character_has_100_hp():
-    character = Character()
-    assert character.hp == 100
