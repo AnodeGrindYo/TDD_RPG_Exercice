ca519d6e0e4324a44e8142872ef85ee9810e608b
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-03-21 16:17:43 +0100

    le perso est vivant quand il est créé

--- character.py
+++ character.py
@@ -1,3 +1,4 @@
 class Character:
     def __init__(self):
         self.hp = 100
+        self.is_alive = True
--- test/test_character.py
+++ test/test_character.py
@@ -4,3 +4,8 @@ from character import Character
 def test_character_hp():
     c = Character()
     assert c.hp == 100
+
+# teste si le character est vivant à sa création
+def test_character_is_alive():
+    c = Character()
+    assert c.is_alive == True
